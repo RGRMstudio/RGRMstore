cffc4baa05b347083ef806f130fadc648b774c0a
RGRMstudio <raguiromo.studio@outlook.com>
2025-12-17 01:54:13 -0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{textAlign: 'center', marginTop: '50px'}}>
+      <h1>Welcome to RaGuiRoMo Store</h1>
+      <p>Bauhaus Inspired Design</p>
+      <a href="/checkout">Go to Checkout</a>
+    </div>
+  )
+}
